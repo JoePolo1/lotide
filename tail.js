7f40071d7a8035ccc29202b1a97c835a1108dba2
Joe Polo <joe.polo@telus.com>
2022-11-14 18:31:15 -0800

    fixed linting indentations

--- tail.js
+++ tail.js
@@ -11,9 +11,9 @@ const assertEqual = function(actual, expected)  {
 
 const tail = function(array) {
   let tailArray = [];
-    for (let i = 1; i < array.length; i++)  {
-      tailArray.push(array[i]);
-    }
+  for (let i = 1; i < array.length; i++)  {
+    tailArray.push(array[i]);
+  }
   console.log(tailArray); //proves out the resulting arrays contents after the loop is complete
   return tailArray;
 };
