8a907cacf4f8778bcea348e5dc6c0842509c63aa
Joe Polo <joe.polo@telus.com>
2022-11-14 16:58:08 -0800

    added a script allowing us to assert console messages when specific errors populate

---
+++ assertEqual.js
@@ -0,0 +1,16 @@
+//Function implementation
+const assertEqual = function(actual, expected)  {
+  let emojiYes = String.fromCodePoint(0x1F7E2);
+  let emojiNo = String.fromCodePoint(0x1F47E);
+  if (actual === expected)  {
+    console.log("Assertion Passed: " + actual + " === " + expected + emojiYes + emojiYes + emojiYes);
+  } else {
+    console.assert(actual === expected, (actual + " !== " + expected + emojiNo + emojiNo + emojiNo));
+  }
+};
+
+//Test code
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Lighthouse Labs", "Lighthouse Labs");
+assertEqual(1, 2);
