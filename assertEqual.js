b6f1c14b4875519951997a8662c4ada5010c7968
Joe Polo <joe.polo@telus.com>
2022-11-14 17:08:40 -0800

    swapped out statement with template literals

--- assertEqual.js
+++ assertEqual.js
@@ -3,9 +3,9 @@ const assertEqual = function(actual, expected)  {
   let emojiYes = String.fromCodePoint(0x1F7E2);
   let emojiNo = String.fromCodePoint(0x1F47E);
   if (actual === expected)  {
-    console.log("Assertion Passed: " + actual + " === " + expected + emojiYes + emojiYes + emojiYes);
+    console.log(`Assertion Passed:  ${actual} === ${expected}` + emojiYes + emojiYes + emojiYes);
   } else {
-    console.assert(actual === expected, (actual + " !== " + expected + emojiNo + emojiNo + emojiNo));
+    console.assert(actual === expected, (`${actual} !== ${expected}` + emojiNo + emojiNo + emojiNo));
   }
 };
 
