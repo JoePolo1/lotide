8346e2ede1e0c90c2f6721eef8b49cb5e668e80c
Joe Polo <joe.polo@telus.com>
2022-11-16 18:53:06 -0800

    uploading eqObjects.js

--- eqObjects.js
+++ eqObjects.js
@@ -1,4 +1,4 @@
-//Function implementation
+//Function implementation that takes actual and expected parameters
 const assertArraysEqual = function(actual, expected)  {
   let emojiYes = String.fromCodePoint(0x1F7E2);
   let emojiNo = String.fromCodePoint(0x1F47E);
@@ -9,6 +9,7 @@ const assertArraysEqual = function(actual, expected)  {
   }
 };
 
+// EQ Arrays function
 const eqArrays = function(array1, array2) {
   for (let i = 0; i < array1.length; i++) {
     if (array1[i] !== array2[i])  {
@@ -19,12 +20,32 @@ const eqArrays = function(array1, array2) {
 };
 
 
+// function for taking in two objects and returns true or false, based on a perfect match.
+// Returns true if both objects have identical keys with identical values.
+// Otherwise you get back a big fat false!
+const eqObjects = function(object1, object2) {
+  const keys1 = Object.keys(object1);
+  const keys2 = Object.keys(object2);
 
-eqArrays([1, 2, 3], [1, 2, 3]); // => true
-eqArrays([1, 2, 3], [3, 2, 1]); // => false
+  if (keys1.length !== keys2.length)  return false;
 
-eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
-eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
+  for (const key of keys1)  {
+    if (Array.isArray(object1[key]) || Array.isArray(object2[key])) {
+      if (eqArrays(object1[key], object2[key])) continue;
+      return false
+    }
+    if (object1[key] !== object2[key]) return false;
+    continue;
+  }
+  return true;
+};
+
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+assertArraysEqual(eqObjects(ab, ba), true); // => true
+
+const abc = { a: "1", b: "2", c: "3" };
+assertArraysEqual(eqObjects(ab, abc), false); // => false
 
 assertArraysEqual(eqArrays([1, 2, 3], [3, 2, 1]), true); // => should fail
 assertArraysEqual(eqArrays([1, 2, "3"], [1, 2, 3]), true); // => Should fail because data type on 3 is different even though ti is semi truthy
