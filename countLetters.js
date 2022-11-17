10d4da51294037811c3fdbc1b0854b471a5d6f25
Joe Polo <joe.polo@telus.com>
2022-11-16 16:25:05 -0800

    updated some checks done by assertEqual

--- countLetters.js
+++ countLetters.js
@@ -1,4 +1,4 @@
-const assertEqual = function (actual, expected) {
+const assertEqual = function(actual, expected) {
   let emojiYes = String.fromCodePoint(0x1F7E2);
   let emojiNo = String.fromCodePoint(0x1F47E);
   if (actual === expected) {
@@ -12,10 +12,11 @@ const countLetters = function(sentence) {
   const trimmedSentence = sentence.replaceAll(" ", "");
   const result = {};
   for (const letter of trimmedSentence) {
-    !result[letter] ? result[letter] = 1 : result[letter]++
+    !result[letter] ? result[letter] = 1 : result[letter]++;
   }
   return result;
 };
 
-
+const checker = countLetters("This is a checkpoint");
+assertEqual(checker.T, 1); // checks how many Capital T's there are in the checker variable, there is one uppercase and one lower.
 console.log(countLetters("Joe Polo"));
