955572bfba3ec346c9b225c85d1270d5edb1a5b9
Joe Polo <joe.polo@telus.com>
2022-11-16 16:18:59 -0800

    counts and checks values of letters

---
+++ countLetters.js
@@ -0,0 +1,21 @@
+const assertEqual = function (actual, expected) {
+  let emojiYes = String.fromCodePoint(0x1F7E2);
+  let emojiNo = String.fromCodePoint(0x1F47E);
+  if (actual === expected) {
+    console.log(emojiYes, emojiYes, emojiYes, `Assertion Passed `, `${actual} \t === \t ${expected}`);
+  } else {
+    console.log(emojiNo, emojiNo, emojiNo, `Assertion Failed `, `${actual} \t !== \t ${expected}`);
+  }
+};
+
+const countLetters = function(sentence) {
+  const trimmedSentence = sentence.replaceAll(" ", "");
+  const result = {};
+  for (const letter of trimmedSentence) {
+    !result[letter] ? result[letter] = 1 : result[letter]++
+  }
+  return result;
+};
+
+
+console.log(countLetters("Joe Polo"));
