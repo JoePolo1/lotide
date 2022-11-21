1a2803211f938f8fae06b4bb36792b4f957891f3
Joe Polo <joe.polo@telus.com>
2022-11-21 14:23:29 -0800

    tail refactor

--- tail.js
+++ tail.js
@@ -5,7 +5,7 @@ const tail = function(array) {
   for (let i = 1; i < array.length; i++)  {
     tailArray.push(array[i]);
   }
-  console.log(tailArray); //proves out the resulting arrays contents after the loop is complete
+  // console.log(tailArray); //proves out the resulting arrays contents after the loop is complete
   return tailArray;
 };
 
